5dbc05252c24ec2eca20325394c70cc55a64af5c
MAXIMILIANO GARBARINO <47433817@ort>
2023-04-25 08:33:17 -0300

    Funcion 1 terminada

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 int opcion;
+List<Cliente> listaClientes= new List<Cliente>();
 do
 {
     opcion = Funciones.IngresarEnteroEnRango("ingrese la opcion (1, 2, 3 o 4) ",1,4);
@@ -8,10 +9,17 @@ do
     case 1:
         int DNI, tipoEntrada,totalAbonado;
         string apellido, nombre;
+
         DateTime fechaInscripcion;
         DNI = Funciones.IngresarEntero("Ingrese DNI del cliente: ");
         apellido = Funciones.IngresarTexto("Ingrese apellido de la persona: ");
         nombre = Funciones.IngresarTexto("Ingrese el nombre del ciente: ");
+        fechaInscripcion = Funciones.IngresarFecha("Ingrese la fecha de inscripcion");
+        tipoEntrada = Funciones.IngresarEnteroEnRango("Ingrese la opcion de la entrada: 1 ($15000),2 ($30000),1 ($10000),1 ($40000)",1,4);
+        int[] precioEntradas = {0,15000,30000,10000,40000};
+        totalAbonado = precioEntradas[tipoEntrada];
+        listaClientes.Add(new Cliente(DNI,apellido,nombre,fechaInscripcion,tipoEntrada,totalAbonado));
+
     break;
     case 2:
     break;
