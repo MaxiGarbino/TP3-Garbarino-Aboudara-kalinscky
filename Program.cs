31aae258b0c0fe3016047eed712a805f64f5ec33
MAXIMILIANO GARBARINO <47433817@ort>
2023-04-25 08:57:50 -0300

    Funcion 1 corregido, funcion 2 en proceso

--- Program.cs
+++ Program.cs
@@ -1,27 +1,15 @@
 ﻿using System.Collections.Generic;
 int opcion;
-List<Cliente> listaClientes= new List<Cliente>();
 do
 {
     opcion = Funciones.IngresarEnteroEnRango("ingrese la opcion (1, 2, 3 o 4) ",1,4);
     switch (opcion)
     {
     case 1:
-        int DNI, tipoEntrada,totalAbonado;
-        string apellido, nombre;
-
-        DateTime fechaInscripcion;
-        DNI = Funciones.IngresarEntero("Ingrese DNI del cliente: ");
-        apellido = Funciones.IngresarTexto("Ingrese apellido de la persona: ");
-        nombre = Funciones.IngresarTexto("Ingrese el nombre del ciente: ");
-        fechaInscripcion = Funciones.IngresarFecha("Ingrese la fecha de inscripcion");
-        tipoEntrada = Funciones.IngresarEnteroEnRango("Ingrese la opcion de la entrada: 1 ($15000),2 ($30000),1 ($10000),1 ($40000)",1,4);
-        int[] precioEntradas = {0,15000,30000,10000,40000};
-        totalAbonado = precioEntradas[tipoEntrada];
-        listaClientes.Add(new Cliente(DNI,apellido,nombre,fechaInscripcion,tipoEntrada,totalAbonado));
-
+        ingresarCliente();
     break;
     case 2:
+        ObtenerEstadisticas();
     break;
     case 3:
         Funciones.IngresarEntero("Ingrese el ID del cliente a buscar: ");
@@ -32,3 +20,21 @@ do
     break;
 }
 }while(opcion != 5);
+
+void ObtenerEstadisticas(){
+    Console.WriteLine("La cantidad de cliente inscriptos son: "+ listaClientes.Count());
+    Console.WriteLine("La cantidad de entradas del tipo 1" + )
+}
+void ingresarCliente(){
+    int DNI, tipoEntrada,totalAbonado;
+    string apellido, nombre;
+
+    DateTime fechaInscripcion;
+    DNI = Funciones.IngresarEntero("Ingrese DNI del cliente: ");
+    apellido = Funciones.IngresarTexto("Ingrese apellido de la persona: ");
+    nombre = Funciones.IngresarTexto("Ingrese el nombre del ciente: ");
+    fechaInscripcion = Funciones.IngresarFecha("Ingrese la fecha de inscripcion");
+    tipoEntrada = Funciones.IngresarEnteroEnRango("Ingrese la opcion de la entrada: 1 ($15000),2 ($30000),1 ($10000),1 ($40000)",1,4);
+    int[] precioEntradas = {0,15000,30000,10000,40000};
+    totalAbonado = precioEntradas[tipoEntrada];
+}
