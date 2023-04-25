c4cc53d4324cc39065f577c23afa9ca194272a8f
MAXIMILIANO GARBARINO <47433817@ort>
2023-04-25 08:10:16 -0300

    Comienzo del menu

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ do
     switch (opcion)
     {
     case 1:
+        int DNI, tipoEntrada,totalAbonado;
+        string apellido, nombre;
+        DateTime fechaInscripcion;
+        DNI = Funciones.IngresarEntero("Ingrese DNI del cliente: ");
+        apellido = Funciones.IngresarTexto("Ingrese apellido de la persona: ");
+        nombre = Funciones.IngresarTexto("Ingrese el nombre del ciente: ");
     break;
     case 2:
     break;
     case 3:
+        Funciones.IngresarEntero("Ingrese el ID del cliente a buscar: ");
     break;
     case 4:
     break;
