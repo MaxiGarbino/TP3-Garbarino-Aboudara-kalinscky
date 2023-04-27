f95e7df34f601817b61ca39415861d4b8a6c4c59
TOMÁS KALINSCKY <47494132@ort>
2023-04-27 15:41:35 -0300

    Terminado

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 int opcion;
+
+int[] precioEntradas = {0,15000,30000,10000,40000};
+
 do
 {
-    opcion = Funciones.IngresarEnteroEnRango("ingrese la opcion (1, 2, 3 o 4) ",1,4);
+    opcion = Funciones.IngresarEnteroEnRango("ingrese la opcion (1, 2, 3, 4 o 5) ",1,5);
     switch (opcion)
     {
     case 1:
@@ -12,9 +15,22 @@ do
         ObtenerEstadisticas();
     break;
     case 3:
-        Funciones.IngresarEntero("Ingrese el ID del cliente a buscar: ");
+        int idBuscado = Funciones.IngresarEntero("Ingrese el ID del cliente a buscar: ");
+        Cliente clienteBuscado = Ticketera.BuscarCliente(idBuscado);
+        if (clienteBuscado.DNI > 0){
+            Console.WriteLine("El nombre del cliente es " + clienteBuscado.nombre);
+            Console.WriteLine("El apellido del cliente es " + clienteBuscado.apellido);
+            Console.WriteLine("El DNI del cliente es " + clienteBuscado.DNI);
+            Console.WriteLine("La fecha de inscripción del cliente fue " + clienteBuscado.fechaInscripcion);
+            Console.WriteLine("El tipo de entrada del cliente es el tipo" + clienteBuscado.tipoEntrada);
+            Console.WriteLine("El total abonado del cliente fue de $" + clienteBuscado.totalAbonado);
+            }
+        else System.Console.WriteLine("El cliente no existe");
     break;
     case 4:
+        int idCambiar = Funciones.IngresarEntero("Ingrese el ID del cliente que quiere cambiar su entrada: ");
+        int tipoEntradaCambio = Funciones.IngresarEntero("Ingrese el tipo de entrada al que desea cambiar: ");
+        Ticketera.CambiarEntrada(idCambiar, tipoEntradaCambio, precioEntradas[tipoEntradaCambio]);
     break;
     case 5:
     break;
@@ -22,20 +38,24 @@ do
 }while(opcion != 5);
 
 void ObtenerEstadisticas(){
-    Console.WriteLine("La cantidad de cliente inscriptos son: "+ Ticketera.DevolverUltimoID());
-    Console.WriteLine("La cantidad de entradas del tipo 1" + )
+    List<string> estadisticas = new List<string>();
+    estadisticas = Ticketera.EstadisticasTicketera();
+    foreach (string msj in estadisticas)
+    {
+        Console.WriteLine(msj);
+    }
 }
 void ingresarCliente(){
     int DNI, tipoEntrada,totalAbonado;
     string apellido, nombre;
 
-    DateTime fechaInscripcion;
+    DateTime fechaInscripcion = DateTime.Today;
+
     DNI = Funciones.IngresarEntero("Ingrese DNI del cliente: ");
     apellido = Funciones.IngresarTexto("Ingrese apellido de la persona: ");
     nombre = Funciones.IngresarTexto("Ingrese el nombre del ciente: ");
-    fechaInscripcion = Funciones.IngresarFecha("Ingrese la fecha de inscripcion");
-    tipoEntrada = Funciones.IngresarEnteroEnRango("Ingrese la opcion de la entrada: 1 ($15000),2 ($30000),1 ($10000),1 ($40000)",1,4);
-    int[] precioEntradas = {0,15000,30000,10000,40000};
+    tipoEntrada = Funciones.IngresarEnteroEnRango("Ingrese la opcion de la entrada: 1 ($15000),2 ($30000),3 ($10000),4 ($40000): ",1,4);
     totalAbonado = precioEntradas[tipoEntrada];
     Ticketera.AgregarCliente(new Cliente(DNI,apellido,nombre,fechaInscripcion,tipoEntrada,totalAbonado));
+    System.Console.WriteLine("El ID de entrada del nuevo cliente es " + Ticketera.DevolverUltimoID());
 }
--- Ticketera.cs
+++ Ticketera.cs
@@ -1,12 +1,12 @@
 static class Ticketera{
-    private static Dictionary<int, Cliente> dicClientes;
+    private static Dictionary<int, Cliente> dicClientes = new Dictionary<int, Cliente>();
     //clave int --> idEntrada       
-    private static int ultimoIDEntrada = 1;
+    private static int ultimoIDEntrada = 0;
     public static int DevolverUltimoID(){
         return ultimoIDEntrada;
     }
     public static int AgregarCliente(Cliente cliente){
-        if (ultimoIDEntrada != 1) ultimoIDEntrada++;
+        ultimoIDEntrada++;
         dicClientes.Add(ultimoIDEntrada, cliente);
         return ultimoIDEntrada;
     }
@@ -22,7 +22,11 @@ static class Ticketera{
         if (dicClientes.ContainsKey(idEntrada)) {
             if (dicClientes[idEntrada].totalAbonado < total) {
                 cambio = true;
+                System.Console.WriteLine("Cambio de tipo de entrada del tipo " + dicClientes[idEntrada].tipoEntrada + "al " + tipo + "Exitoso.");
+                System.Console.WriteLine("Monto agregado: $" + (total - dicClientes[idEntrada].totalAbonado));
                 dicClientes[idEntrada].tipoEntrada = tipo;
+                dicClientes[idEntrada].totalAbonado = total;
+
             }
             else Console.WriteLine("No se pudo hacer el cambio porque el monto del tipo de entrada al que desea cambiar no es mayor al anterior");
         }
@@ -31,8 +35,23 @@ static class Ticketera{
     }
     public static List<string> EstadisticasTicketera(){
         List<string> estadisticas = new List<string>();
-        estadisticas.Add(dicClientes.Count().ToString());
+        estadisticas.Add("Hay " + dicClientes.Count() + " clientes inscriptos");
+        int[] cantEntradaXTipo = {0,0,0,0,0};
+        int[] recaudacionXTipo = {0,0,0,0,0};
+        foreach (int id in dicClientes.Keys)
+        {
+            cantEntradaXTipo[dicClientes[id].tipoEntrada]++;
+            recaudacionXTipo[dicClientes[id].tipoEntrada] += dicClientes[id].totalAbonado;
+        }
+        int recaudacionTotal = 0;
+        double cantClientes = dicClientes.Count;
+        for (int i = 1; i < recaudacionXTipo.Length; i++)
+        {
+            recaudacionTotal += recaudacionXTipo[i];
+            estadisticas.Add ("El porcentaje de entradas del tipo " + i + " es " + (cantEntradaXTipo[i]*100 / cantClientes).ToString()+ "%");
+            estadisticas.Add("La recudacion del tipo " + i + " es "+ recaudacionXTipo[i]);
+        }
+        estadisticas.Add("La recaudación total fue de $" + recaudacionTotal);
         return estadisticas;
-        
     }
 }
